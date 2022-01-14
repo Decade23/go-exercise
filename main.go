5b5af525c2929ba669cf4e51c952449f09c27c27
Dedi Fardiyanto <dedifardiyanto@yahoo.co.id>
2022-01-14 16:39:56 +0700

    how to use struct

--- main.go
+++ main.go
@@ -1,58 +1,69 @@
 package main
 
 import (
-	"strconv"
 	"fmt"
 	) 
 
+	type User struct {
+		ID int
+		FirstName string
+		LastName string
+		Email string
+		IsActive bool
+	}
+
+	type Group struct {
+		Name string
+		Admin User
+		Users []User
+		IsAvailable bool
+	}
 func main() {
-	var name string = "Dedi Fardiyanto"
-	age := 23
-	fmt.Println("Hello World " + name + " Age: " + strconv.Itoa(age)) 
+	// // initialize struct of User
+	user := User{}
 	
-	// loop
-	// for normal
-	// for i := 1; i <= 100; i ++ {
-	// 	fmt.Println("im learn of go : ", i)
-	// 	if i == 100 {
-	// 		fmt.Println("------------------------------------------")
-	// 	}
-	// }
+	// // method insert data into struct 1
+	user.ID = 1
+	user.FirstName = "Dedi"
+	user.LastName = "Fardiyanto"
+	user.Email = "dedif15@gmail.com"
+	user.IsActive = true
 
-	// for style while
-	// a := 1
-	// for a <= 100 {
-	// 	fmt.Println("learn of go : ", a)
-	// 	a++
-	// }
+	// // method insert data into struct 2
+	user2 := User{ID: 2, FirstName: "Dedi", LastName: "Fard", Email: "dedif15@gmail.com", IsActive: true}
+	fmt.Println("User Struct-------------------------------------------------------")
+	fmt.Println(user)
+	fmt.Println(user2)
+	fmt.Println("-------------------------------------------------------")
 
-	// for style ForEach
-	// sentence := "here we are learning of GO"
-	// for i, l := range sentence {
-	// 	fmt.Println("index: ", i, " letter: ", string(l))
-	// }
+	fmt.Println(displayName(user))
+	fmt.Println(displayName(user2))
 
-	// for i, l := range sentence {
-		// ls := string(l)
-		// using modulus
-		// if i % 2 != 0 {
-		// 	fmt.Println("index : ", i, " letter: ", ls)
-		// }
+	fmt.Println("Group Struct-------------------------------------------------------")
+	users := []User{user, user2}
+	group := Group{Name: "Group 1", Admin: user, Users: users, IsAvailable: true}
 
-		// filter print by text vocal
-		// switch ls {
-		// case "a", "i", "u", "e", "o":
-		// 	fmt.Println("index : ", i, " letter: ", ls)
-		// }
-	// }
+	displayGroup(group)
+}
 
-	// array
-	arr := [...]string {
-		"c", "c#", "go", "php", "javascript",
+// struct as params func
+func displayName(user User) string {
+	return fmt.Sprintf("Name: %s %s, Email: %s", user.FirstName, user.LastName, user.Email)
+}
+
+func displayGroup(group Group) {
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Printf("Name Of Group: %s", group.Name)
+	fmt.Println("")
+	fmt.Printf("Admin Of Group: %s", group.Admin.FirstName)
+	fmt.Println("")
+	fmt.Printf("Member Count: %d", len(group.Users))
+	
+	fmt.Println("")
+	fmt.Println("")
+	fmt.Println("User Of Group: ")
+	for _, u := range group.Users {
+	fmt.Println(u.FirstName + " " + u.LastName)
 	}
-	fmt.Println("array length : ", len(arr))
-	for idx, let := range arr {
-		fmt.Println("idx : ", idx, " word: ", let)
-	}
-
 }
