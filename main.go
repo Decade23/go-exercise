49f9dd1fa4aa3693320ee597eb04b9c562c335cc
Dedi Fardiyanto <dedifardiyanto@yahoo.co.id>
2022-01-24 23:25:02 +0700

    interfae

--- main.go
+++ main.go
@@ -1,58 +1,41 @@
 package main
 
 import (
-	"strconv"
 	"fmt"
 	) 
 
-func main() {
-	var name string = "Dedi Fardiyanto"
-	age := 23
-	fmt.Println("Hello World " + name + " Age: " + strconv.Itoa(age)) 
-	
-	// loop
-	// for normal
-	// for i := 1; i <= 100; i ++ {
-	// 	fmt.Println("im learn of go : ", i)
-	// 	if i == 100 {
-	// 		fmt.Println("------------------------------------------")
-	// 	}
-	// }
-
-	// for style while
-	// a := 1
-	// for a <= 100 {
-	// 	fmt.Println("learn of go : ", a)
-	// 	a++
-	// }
-
-	// for style ForEach
-	// sentence := "here we are learning of GO"
-	// for i, l := range sentence {
-	// 	fmt.Println("index: ", i, " letter: ", string(l))
-	// }
-
-	// for i, l := range sentence {
-		// ls := string(l)
-		// using modulus
-		// if i % 2 != 0 {
-		// 	fmt.Println("index : ", i, " letter: ", ls)
-		// }
-
-		// filter print by text vocal
-		// switch ls {
-		// case "a", "i", "u", "e", "o":
-		// 	fmt.Println("index : ", i, " letter: ", ls)
-		// }
-	// }
-
-	// array
-	arr := [...]string {
-		"c", "c#", "go", "php", "javascript",
-	}
-	fmt.Println("array length : ", len(arr))
-	for idx, let := range arr {
-		fmt.Println("idx : ", idx, " word: ", let)
-	}
-
+type BangunDatar interface {
+	HitungLuas() int
 }
+
+type Persegi struct {
+	Sisi int
+}
+
+type PersegiPanjang struct {
+	Panjang int
+	Lebar int
+}
+
+func(persegiPanjang PersegiPanjang) HitungLuas() int {
+	return persegiPanjang.Panjang * persegiPanjang.Lebar
+}
+
+func (persegi Persegi) HitungLuas() int {
+	return persegi.Sisi * persegi.Sisi
+}
+
+
+func main() {
+	persegi := Persegi{Sisi: 9}
+	fmt.Println("Hitung Luas :" , SeberapaLuas(persegi))
+	
+	fmt.Println("--------------------------------------")
+
+	persegiPanjang := PersegiPanjang{Panjang: 10, Lebar: 5}
+	fmt.Println("Hitung Luas :" , SeberapaLuas(persegiPanjang))
+}
+
+func SeberapaLuas(bidangDatar BangunDatar) int {
+	return bidangDatar.HitungLuas()
+} 
