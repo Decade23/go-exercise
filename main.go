4c5ba94687e8abdc4a7ff715778b8a8236bfb75a
Dedi Fardiyanto <dedifardiyanto@yahoo.co.id>
2022-01-24 22:40:09 +0700

    ...

--- main.go
+++ main.go
@@ -1,58 +1,29 @@
 package main
 
-import (
-	"strconv"
-	"fmt"
-	) 
+import "fmt"
+
+type Sentence struct {
+	NumberA   int
+	Sentences []string
+}
+
+func (sentence Sentence) display() {
+	fmt.Printf("No: %d", sentence.NumberA)
+	for _, v := range sentence.Sentences {
+		fmt.Printf("panjang %s karakter dari kalimat ini: ", v)
+	}
+}
 
 func main() {
-	var name string = "Dedi Fardiyanto"
-	age := 23
-	fmt.Println("Hello World " + name + " Age: " + strconv.Itoa(age)) 
-	
-	// loop
-	// for normal
-	// for i := 1; i <= 100; i ++ {
-	// 	fmt.Println("im learn of go : ", i)
-	// 	if i == 100 {
-	// 		fmt.Println("------------------------------------------")
-	// 	}
-	// }
+	numberA := 10
+	numberB := &numberA
 
-	// for style while
-	// a := 1
-	// for a <= 100 {
-	// 	fmt.Println("learn of go : ", a)
-	// 	a++
-	// }
+	// change value of numberB shall effect to  numberA because its change the value of computerized
+	*numberB = 20
+	fmt.Println("number A: ", numberA)
+	fmt.Println("---------------")
+	fmt.Println("number B: ", *numberB)
 
-	// for style ForEach
-	// sentence := "here we are learning of GO"
-	// for i, l := range sentence {
-	// 	fmt.Println("index: ", i, " letter: ", string(l))
-	// }
-
-	// for i, l := range sentence {
-		// ls := string(l)
-		// using modulus
-		// if i % 2 != 0 {
-		// 	fmt.Println("index : ", i, " letter: ", ls)
-		// }
-
-		// filter print by text vocal
-		// switch ls {
-		// case "a", "i", "u", "e", "o":
-		// 	fmt.Println("index : ", i, " letter: ", ls)
-		// }
-	// }
-
-	// array
-	arr := [...]string {
-		"c", "c#", "go", "php", "javascript",
-	}
-	fmt.Println("array length : ", len(arr))
-	for idx, let := range arr {
-		fmt.Println("idx : ", idx, " word: ", let)
-	}
+	fmt.Println("---------------")
 
 }
